b834f8c70461e8c2a98dd1f0aa418d543e33b95c
Justin Speers & mark rubenstein <student@epicodus.com>
2014-07-24 10:20:50 -0700

    create appropriate_e_recursion, rename appropriate_e_no_recursion

--- lib/appropriate_e.rb
+++ lib/appropriate_e.rb
@@ -1,5 +1,13 @@
+def factorial(n)
+  if n == 0
+    1
+  else
+    n * factorial(n-1)
+  end
+end
 
-def appropriate_e(depth)
+
+def appropriate_e_no_recursion(depth)
   e = 1.0
 
   (1..depth).each do |count|
@@ -14,4 +22,19 @@ def appropriate_e(depth)
 
 end
 
-puts appropriate_e(200000)
+
+
+def appropriate_e_recursion(depth)
+  e = 1.0
+
+  (1..depth).each do |count|
+    # denominator = 1
+    # (1..count).each do |number|
+    #   denominator *= number
+    # end
+    e += 1.0 / factorial(count)
+  end
+
+  e
+
+end
--- spec/appropriate_e_spec.rb
+++ spec/appropriate_e_spec.rb
@@ -2,8 +2,12 @@ require('rspec')
 require('appropriate_e')
 
 describe("appropriate_e") do
-  it("calculates e to 2.71828") do
-    (appropriate_e(20) > 2.7 && appropriate_e(20) < 2.72).should(eq(true))
+
+  it("calculates e to ~2.71828 without recursion") do
+    (appropriate_e_no_recursion(200) > 2.7182 && appropriate_e_no_recursion(200) < 2.72183).should(eq(true))
   end
 
+  it("calculate e to ~2.71828 with recursion") do
+    (appropriate_e_recursion(200) > 2.7182 && appropriate_e_recursion(200) < 2.72183).should(eq(true))
+  end
 end
